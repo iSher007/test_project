24f8c4356a635cbd9cd868ebca02a047faf62bb1
Sher Dadakhanov <68942832+iSher007@users.noreply.github.com>
2022-08-23 11:59:57 +0600

    update

--- market/urls.py
+++ market/urls.py
@@ -1,6 +1,5 @@
-from django.urls import path, include
+from django.urls import path
 from .views import *
-
 
 urlpatterns = [
     path('<int:shop_id>/categories/<int:category_id>/', index, name='index'),
